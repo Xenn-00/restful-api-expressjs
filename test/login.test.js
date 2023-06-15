adfb71c2b8adfad40efefe4664122fe4c2efa511
Xenon-00 <faqihfirmansyah350@gmail.com>
2023-06-15 17:47:43 +0700

    testing endpoints

---
+++ test/login.test.js
@@ -0,0 +1,69 @@
+import supertest from "supertest"
+import { createTestUser, removeTestUser } from "./test-util.js"
+import { web } from "../src/app/web.js"
+import { logger } from "../src/app/logging.js"
+
+describe('POST /api/users/login', function () {
+    beforeEach(async () => {
+        await createTestUser()
+    })
+
+    afterEach(async () => {
+        await removeTestUser()
+    })
+
+    it('should be can login', async () => {
+        const result = await supertest(web)
+            .post("/api/users/login")
+            .send({
+                username: "Fuma",
+                password: "Wazxse34"
+            })
+
+        logger.info(result.body)
+        expect(result.status).toBe(200)
+        expect(result.body.data.token).toBeDefined()
+        expect(result.body.data.token).not.toBe("test")
+    })
+
+    it('should reject login if request is invalid', async () => {
+        const result = await supertest(web)
+            .post("/api/users/login")
+            .send({
+                username: "",
+                password: "Wazxse34"
+            })
+
+        logger.info(result.body)
+        expect(result.status).toBe(400)
+        expect(result.body.errors).toBeDefined()
+    })
+
+    it('should reject login if password is wrong', async () => {
+        const result = await supertest(web)
+            .post("/api/users/login")
+            .send({
+                username: "Fuma",
+                password: "Wazxs34"
+            })
+
+        logger.info(result.body)
+        expect(result.status).toBe(401)
+        expect(result.body.errors).toBeDefined()
+    })
+
+    it('should reject login if username is wrong', async () => {
+        const result = await supertest(web)
+            .post("/api/users/login")
+            .send({
+                username: "Uproar",
+                password: "Wazxse34"
+            })
+
+        logger.info(result.body)
+        expect(result.status).toBe(401)
+        expect(result.body.errors).toBeDefined()
+    })
+
+
+})
---
+++ test/register.test.js
@@ -0,0 +1,67 @@
+import supertest from "supertest"
+import { web } from "../src/app/web.js"
+import { logger } from "../src/app/logging.js"
+import { createTestUser, removeTestUser } from "./test-util.js"
+
+describe('POST /api/users', function () {
+    beforeEach(async () => {
+        await createTestUser()
+    })
+
+    afterEach(async () => {
+        await removeTestUser()
+    })
+
+    it('should can register new user', async () => {
+        const result = await supertest(web)
+            .post("/api/users")
+            .send({
+                username: "Fuma",
+                password: "Wazxse34",
+                name: "Fuma zako"
+            })
+        expect(result.status).toBe(200)
+        expect(result.body.data.username).toBe("Fuma")
+        expect(result.body.data.name).toBe("Fuma zako")
+        expect(result.body.data.password).toBeUndefined()
+    })
+
+
+    it('should reject if request invalid', async () => {
+        const result = await supertest(web)
+            .post("/api/users")
+            .send({
+                username: "",
+                password: "",
+                name: "Fuma zako"
+            })
+        logger.info(result.body)
+        expect(result.status).toBe(400)
+        expect(result.body.errors).toBeDefined()
+    })
+
+    it('should reject if username already registered', async () => {
+        let result = await supertest(web)
+            .post("/api/users")
+            .send({
+                username: "Fuma",
+                password: "Wazxse34",
+                name: "Fuma zako"
+            })
+        expect(result.status).toBe(200)
+        expect(result.body.data.username).toBe("Fuma")
+        expect(result.body.data.name).toBe("Fuma zako")
+        expect(result.body.data.password).toBeUndefined()
+
+        result = await supertest(web)
+            .post("/api/users")
+            .send({
+                username: "Fuma",
+                password: "Wazxse34",
+                name: "Fuma zako"
+            })
+        logger.info(result.body)
+        expect(result.status).toBe(400)
+        expect(result.body.errors).toBeDefined()
+    })
+})
