c0b1f8257a7b6da8d13a93c2f0dff818ae734ca1
Xenon-00 <faqihfirmansyah350@gmail.com>
2023-06-15 17:48:41 +0700

    add login validation

--- src/validation/user.validation.js
+++ src/validation/user.validation.js
@@ -2,10 +2,16 @@ import Joi from "joi"
 
 const registerUserValidation = Joi.object({
     username: Joi.string().max(100).required(),
-    password: Joi.string().max(100).required(),
+    password: Joi.string().min(6).max(100).required(),
     name: Joi.string().max(100).required()
 })
 
+const loginUserValidation = Joi.object({
+    username: Joi.string().max(100).required(),
+    password: Joi.string().min(6).max(100).required()
+})
+
 export {
-    registerUserValidation
+    registerUserValidation,
+    loginUserValidation
 }
