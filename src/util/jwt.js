41616dedfa74f65b27df4cd7dad3b18abbb51ab4
Xenon-00 <faqihfirmansyah350@gmail.com>
2023-06-15 17:49:40 +0700

    create token

---
+++ src/util/jwt.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken"
+import { ResponseError } from "../error/response.error"
+
+const sign = (user, secret_token) => {
+    const accessToken = jwt.sign(user, secret_token, { expiresIn: "7d" })
+    return accessToken
+}
+
+const verify = (req, res, next) => {
+    const authHeader = req.headers['Authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if (!token) throw new ResponseError(401, "Unauthorized")
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) throw new ResponseError(403, "Unauthorized")
+        req.user = user
+        next()
+    })
+}
+
+export {
+    sign,
+    verify
+}
