ab5727e8a53121ee39ae31da65005fc990cf9367
Xenn-00 <node.xenon@gmail.com>
2023-06-16 11:14:54 +0700

    validating for get user

--- src/validation/user.validation.js
+++ src/validation/user.validation.js
@@ -11,7 +11,10 @@ const loginUserValidation = Joi.object({
     password: Joi.string().min(6).max(100).required()
 })
 
+const getUserValidation = Joi.string().max(100).required()
+
 export {
     registerUserValidation,
-    loginUserValidation
+    loginUserValidation,
+    getUserValidation
 }
