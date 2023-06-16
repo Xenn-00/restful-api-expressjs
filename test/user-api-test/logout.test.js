17609f3c9f1c3e5dac9fd5ad677779f5ee9c9372
Xenn-00 <node.xenon@gmail.com>
2023-06-16 14:10:39 +0700

    logout api testing

---
+++ test/user-api-test/logout.test.js
@@ -0,0 +1,29 @@
+import supertest from "supertest"
+import { createTestUser, getTestUser, loginTestUser, removeTestUser } from "./test-util.js"
+import { web } from "../../src/app/web.js"
+import { logger } from "../../src/app/logging.js"
+
+
+describe("DELETE /api/users/logout", function () {
+    beforeEach(async () => {
+        await createTestUser()
+    })
+
+    afterEach(async () => {
+        await removeTestUser()
+    })
+
+    it("should can logout", async function () {
+        const token = await loginTestUser()
+        const result = await supertest(web)
+            .delete('/api/users/logout')
+            .set("Authorization", token)
+
+        logger.info(result.body)
+        expect(result.status).toBe(200)
+        expect(result.body.data).toBe("OK")
+
+        const user = await getTestUser()
+        expect(user.token).toBeNull()
+    })
+})
