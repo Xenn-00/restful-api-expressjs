b81b4da19906644e011aadeaaddb524068948c8b
Xenn-00 <node.xenon@gmail.com>
2023-06-16 11:12:37 +0700

    implement user router

---
+++ src/route/api.js
@@ -0,0 +1,13 @@
+import express from "express"
+import userController from "../controller/user.controller.js"
+import { authMiddleware } from "../middleware/auth.middleware.js"
+
+const userRouter = express.Router()
+
+userRouter.use(authMiddleware)
+userRouter.get("/api/users/current", userController.get)
+
+
+export {
+    userRouter
+}
