6446d7b2b789f10fecb2b3691992c60d26e18028
Xenn-00 <node.xenon@gmail.com>
2023-06-17 08:04:50 +0700

    move file

---
+++ test/test-util.js
@@ -0,0 +1,100 @@
+import { primaclient } from "../src/app/database.js"
+import jwt from "jsonwebtoken"
+
+import bcrypt from "bcrypt"
+
+const removeTestUser = async () => {
+    await primaclient.user.deleteMany({
+        where: {
+            username: "Fuma"
+        }
+    })
+}
+
+const createTestUser = async () => {
+    await primaclient.user.create({
+        data: {
+            username: "Fuma",
+            password: await bcrypt.hash("Wazxse34", 10),
+            name: "Fuma zakko",
+            token: "test"
+        }
+    })
+}
+
+const loginTestUser = async () => {
+    const user = {
+        username: "Fuma",
+        password: "Wazxse34"
+    }
+    const login = await primaclient.user.findUnique({
+        where: {
+            username: user.username
+        },
+        select: {
+            username: true,
+            password: true
+        }
+    })
+    const isPasswordValid = await bcrypt.compare(user.password, login.password)
+    if (!isPasswordValid) throw new ResponseError(401, "Username or password wrong")
+    const token = jwt.sign({ username: login.username }, process.env.ACCESS_TOKEN_SECRET)
+    const result = await primaclient.user.update({
+        data: {
+            token: token
+        },
+        where: {
+            username: login.username
+        },
+        select: {
+            token: true
+        }
+    })
+
+    return result.token
+}
+
+const getTestUser = async () => {
+    return primaclient.user.findUnique({
+        where: {
+            username: "Fuma"
+        }
+    })
+}
+
+const removeAllTestContacts = async () => {
+    await primaclient.contact.deleteMany({
+        where: {
+            username: "Fuma"
+        }
+    })
+}
+
+const createTestContact = async () => {
+    await primaclient.contact.create({
+        data: {
+            username: "Fuma",
+            first_name: "Fuma",
+            last_name: "zakko",
+            email: "fumaZakko@hololiven.com",
+            phone: "+81267122387"
+        }
+    })
+}
+const getTestContact = async () => {
+    return primaclient.contact.findFirst({
+        where: {
+            username: "Fuma"
+        },
+    })
+}
+
+export {
+    removeTestUser,
+    createTestUser,
+    loginTestUser,
+    getTestUser,
+    createTestContact,
+    getTestContact,
+    removeAllTestContacts
+}
