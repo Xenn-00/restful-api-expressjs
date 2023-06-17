019dd602396546c30e29c28124c47204972b6056
Xenn-00 <node.xenon@gmail.com>
2023-06-17 14:59:02 +0700

    create update and search validation

--- src/validation/contact.validation.js
+++ src/validation/contact.validation.js
@@ -9,7 +9,26 @@ const createContactValidation = Joi.object({
 
 const getContactValidation = Joi.string().guid().required()
 
+
+const updateContactValidation = Joi.object({
+    id: Joi.string().guid().required(),
+    first_name: Joi.string().max(100).required(),
+    last_name: Joi.string().max(100).optional(),
+    email: Joi.string().max(200).email().optional(),
+    phone: Joi.string().max(20).optional(),
+})
+
+const searchContactValidation = Joi.object({
+    page: Joi.number().min(1).positive().default(1),
+    size: Joi.number().min(1).positive().max(50).default(10),
+    name: Joi.string().optional(),
+    email: Joi.string().email().optional(),
+    phone: Joi.string().optional()
+})
+
 export {
     createContactValidation,
-    getContactValidation
+    getContactValidation,
+    updateContactValidation,
+    searchContactValidation
 }
