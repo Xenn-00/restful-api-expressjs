5f574bd59304e535e33c4111a1394abe3d7b2d12
Xenn-00 <node.xenon@gmail.com>
2023-06-18 14:18:37 +0700

    add get, update, list, and remove spec

--- src/controller/address.controller.js
+++ src/controller/address.controller.js
@@ -14,6 +14,66 @@ const create = async (req, res, next) => {
     }
 }
 
+const get = async (req, res, next) => {
+    try {
+        const user = req.user
+        const addressId = req.params.addressId
+        const contactId = req.params.contactId
+        const result = await addressService.get(user, contactId, addressId)
+        res.status(200).json({
+            data: result
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
+const update = async (req, res, next) => {
+    try {
+        const user = req.user
+        const contactId = req.params.contactId
+        const addressId = req.params.addressId
+        const request = req.body
+        request.id = addressId
+        const result = await addressService.update(user, contactId, request)
+        res.status(200).json({
+            data: result
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+const list = async (req, res, next) => {
+    try {
+        const user = req.user
+        const contactId = req.params.contactId
+        const result = await addressService.list(user, contactId)
+        res.status(200).json({
+            data: result
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
+const remove = async (req, res, next) => {
+    try {
+        const user = req.user
+        const contactId = req.params.contactId
+        const addressId = req.params.addressId
+        await addressService.remove(user, contactId, addressId)
+        res.status(200).json({
+            data: "OK"
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
 export default {
-    create
+    create,
+    get,
+    update,
+    list,
+    remove
 }
