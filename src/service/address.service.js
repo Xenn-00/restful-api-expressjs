bc8ad26b8a7be70ce55868061b69f4a50a6d4150
Xenn-00 <node.xenon@gmail.com>
2023-06-18 14:20:16 +0700

    adding new util and finishing specs

--- src/service/address.service.js
+++ src/service/address.service.js
@@ -2,10 +2,10 @@ import { primaclient } from "../app/database.js"
 import { getContactValidation } from "../validation/contact.validation.js"
 import { validate } from "../validation/validation.js"
 import { ResponseError } from "../error/response.error.js"
-import { createAddressValidation } from "../validation/address.validation.js"
+import { createAddressValidation, getAddressValidation, updateAddressValidation } from "../validation/address.validation.js"
 import crypto from "crypto"
 
-const create = async (user, contactId, request) => {
+const checkContactExist = async (user, contactId) => {
     contactId = validate(getContactValidation, contactId)
     const countCountact = await primaclient.contact.count({
         where: {
@@ -14,6 +14,12 @@ const create = async (user, contactId, request) => {
         }
     })
     if (countCountact !== 1) throw new ResponseError(404, "Contact is not found")
+
+    return contactId
+}
+
+const create = async (user, contactId, request) => {
+    contactId = await checkContactExist(user, contactId)
 
     const address = validate(createAddressValidation, request)
     address.contact_id = contactId
@@ -31,6 +37,82 @@ const create = async (user, contactId, request) => {
     })
 }
 
+const get = async (user, contactId, addressId) => {
+    contactId = await checkContactExist(user, contactId)
+    addressId = validate(getAddressValidation, addressId)
+
+    const address = await primaclient.address.findFirst({
+        where: {
+            contact_id: contactId,
+            id: addressId
+        },
+        select: {
+            id: true,
+            street: true,
+            city: true,
+            province: true,
+            country: true,
+            postal_code: true
+        }
+    })
+
+    if (!address) throw new ResponseError(404, "Address is not found")
+    return address
+}
+
+const update = async (user, contactId, request) => {
+    contactId = await checkContactExist(user, contactId)
+    const address = validate(updateAddressValidation, request)
+    return primaclient.address.update({
+        where: {
+            id: address.id
+        },
+        data: {
+            street: address.street,
+            city: address.city,
+            province: address.province,
+            country: address.country,
+            postal_code: address.postal_code
+        },
+        select: {
+            id: true,
+            street: true,
+            city: true,
+            province: true,
+            country: true,
+            postal_code: true
+        }
+    })
+}
+
+const list = async (user, contactId) => {
+    contactId = await checkContactExist(user, contactId)
+
+    const address = await primaclient.address.findMany({
+        where: {
+            contact: {
+                username: user.username
+            }
+        }
+    })
+    return address
+}
+
+const remove = async (user, contactId, addressId) => {
+    contactId = await checkContactExist(user, contactId)
+    addressId = validate(getAddressValidation, addressId)
+
+    return primaclient.address.delete({
+        where: {
+            id: addressId
+        }
+    })
+}
+
 export default {
-    create
+    create,
+    get,
+    update,
+    list,
+    remove
 }
